263b48c33e30b14c1ef376511e7ab2ecdb184128
santhoshp5 <53000964+santhoshp5@users.noreply.github.com>
2019-10-14 10:52:58 +0200

    Update HelloController.java

--- src/main/java/com/dockerforjavadevelopers/hello/HelloController.java
+++ src/main/java/com/dockerforjavadevelopers/hello/HelloController.java
@@ -11,9 +11,6 @@ public class HelloController {
     public String index() {
         System.out.println("ererererererererer");
         System.out.println("sdfsdfsdf");
-        System.out.println("sampleeeeee");
-         System.out.println("ererererererererer");
-        System.out.println("sdfsdfsdf");
         return "Hello World\n";
     }
     
